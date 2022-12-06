32910ae11c592232e803f8284b9eee1a1d2f810b
Brent <jbmsharpe@gmail.com>
2022-12-06 11:10:25 -0500

    created a new page for create list button, and created cards to be created once user inputs a title to create a new list. Added title and edit button to cards displayed on home page

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
-const input = document.querySelector('.input');
-const button = document.querySelector('.button');
+const listItemInput = document.querySelector('.list-item-input');
+const addToListButton = document.querySelector('.add-to-list-button');
 const table = document.getElementById('to-do-list');
 
-// user inputs items
+// STEP 1 >>>>>> user clicks on create-a-list button
+const modal = document.getElementById('modal');
+const createListButton = document.querySelector('.create-a-list-button');
+
+   // >>>>>> modal is opened when button is clicked
+createListButton.addEventListener('click', function() {
+    modal.style.display = 'block'
+})
 
 
 
-// user clicks submit
-button.addEventListener('click', function (){
-    //displays content in table
-    let userInput = input.value;
+// Step 2 >>>>>>> user inputs title of list and clicks create list, user is taken to home page
+  //  where new list is there on a card
+
+const titleInput = document.querySelector('.title-input');
+const submitListTitleButton = document.querySelector(".submit-list-title-button")
+
+submitListTitleButton.addEventListener('click', newList)
+
+function newList() {
+    const newCard = document.createElement('div');
+    newCard.classList.add('card');
+
+    let userTitleInput = titleInput.value;
+
+    const newTitle = document.createElement('h3');
+    newTitle.innerHTML = `${userTitleInput}`;
+
+    newCard.appendChild(newTitle);
+    document.getElementById('body').appendChild(newCard)
+
+    modal.style.display = 'none'
+    titleInput.value = ''
+
+}
+
+
+
+
+
+
+
+// user clicks add to list, list item gets added to the list
+addToListButton.addEventListener('click', function(){
+    let userInput = listItemInput.value;
     const toDoItem = document.createElement('tr');
-    const deleteButton = document.createElement('button');
+    const deleteButton = document.createElement('button')
+    deleteButton.innerHTML = 'X'
     deleteButton.classList.add('delete-button');
     toDoItem.classList.add('to-do-item');
 
-
+    //display use input in list
     toDoItem.innerHTML = `
     <td>${userInput}</td>`;
-
-    // deleteButton.innerHTML = `<td><td>`
-    
 
     table.appendChild(toDoItem);
     toDoItem.appendChild(deleteButton);
 
-    deleteButton.addEventListener('click', function() {
+     // clear fields
+     listItemInput.value = '';
+
+
+    // delete items
+     deleteButton.addEventListener('click', function() {
         toDoItem.remove();
     })
 
-    // clear fields
-    input.value = '';
 })
-
