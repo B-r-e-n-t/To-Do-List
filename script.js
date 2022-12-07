b52ad1de0f7f1656b337fe70d3bebffa8b9d6d11
Brent <jbmsharpe@gmail.com>
2022-12-07 10:59:32 -0500

    added event listener to card to open a modal to edit/view the list

--- script.js
+++ script.js
@@ -13,35 +13,57 @@ createListButton.addEventListener('click', function() {
 
 
 
-// Step 2 >>>>>>> user inputs title of list and clicks create list, user is taken to home page
-  //  where new list is there on a card
+// Step 2 >>>>>>> user inputs title of list and clicks create list, modal disappears
+  //  and new list is on a card on home page
 
 const titleInput = document.querySelector('.title-input');
 const submitListTitleButton = document.querySelector(".submit-list-title-button")
+// card is created
+const newCard = document.createElement('div');
+newCard.classList.add('card');
 
 submitListTitleButton.addEventListener('click', newList)
 
 function newList() {
-    const newCard = document.createElement('div');
-    newCard.classList.add('card');
 
+    // title is created
     let userTitleInput = titleInput.value;
-
     const newTitle = document.createElement('h3');
     newTitle.innerHTML = `${userTitleInput}`;
+    newTitle.classList.add('title')
 
+    //edit button is created
+    const editButton = document.createElement('button');
+    editButton.innerHTML = 'view/edit';
+    editButton.classList.add('edit-button')
+
+    // append title and edit button to card, append card to body (home page)
     newCard.appendChild(newTitle);
+    newCard.appendChild(editButton);
     document.getElementById('body').appendChild(newCard)
 
+    // user input in modal and modal dissappear
     modal.style.display = 'none'
     titleInput.value = ''
-
 }
 
 
+// STEP 3 >>>>>>>>>>>> user clicks on card to edit or view, card shows up on modal and user edits list
+    newCard.addEventListener('click', function() {
+            document.getElementById('modal-list').style.display = 'block'
+    })
+
+// STEP 4 user saves list and modal closes  
+    
 
 
 
+
+
+
+
+
+// adding list items to each individual list
 
 
 // user clicks add to list, list item gets added to the list
